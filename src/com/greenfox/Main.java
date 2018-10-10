fa96145c7c46586ac12f9a7564559ec3c0b25721
Szilvia Toth <tothszilvia88@gmail.com>
2018-10-10 13:30:12 +0200

    a1

--- src/com/greenfox/Main.java
+++ src/com/greenfox/Main.java
@@ -2,7 +2,12 @@ package com.greenfox;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    greet();
+
+  }
+
+  public static void greet(){
+    System.out.println("Szilvi");
+  }
 }
