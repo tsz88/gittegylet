60ea0196bb7bb1a9f273afd37628803b5ec327c3
ladyvengeance <csilla.deszpoth@gmail.com>
2018-10-10 14:27:20 +0200

    b4

--- src/com/greenfox/Age.java
+++ src/com/greenfox/Age.java
@@ -2,12 +2,8 @@ package com.greenfox;
 
 public class Age {
 
-<<<<<<< HEAD
-  public void count() {
-    System.out.println("December");
-=======
   public void count(int input) {
     System.out.println(input);
->>>>>>> 305e50ee5b7ade0ed7c8cfb4a7c3e714ac79d373
+
   }
 }
